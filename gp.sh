0e291cec49ac40a7196cd213cdaeee2cc56e098c
Caleb Smith <karagenit@outlook.com>
2018-08-08 13:35:42 -0400

    Store Input String

--- gp.sh
+++ gp.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+reset=$(tput sgr0)
+dim=$(tput dim)
+
+bins=$(compgen -c)
+input=""
+
 while true; do
 
     read -rsn1 char
-    bins=$(compgen -c | grep "^${char}")
-    first=$(echo "$bins" | head -n 1)
-    echo $first
+
+    # TODO: handle backspace?
+
+    input="${input}${char}"
+
+    matches=$(echo "$bins" | grep "^${input}")
+    first=$(echo "$matches" | head -n 1)
+
+    firstrest=${first#"$input"}
+
+    echo -ne "${input}${firstrest}\t\t\t\r"
 done
