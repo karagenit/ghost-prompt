e3076ddbf01471e7b4abf68c0c5be3518b98ae66
Caleb Smith <karagenit@outlook.com>
2018-08-23 15:43:36 -0400

    Cleanup Line Clearing

--- gp.sh
+++ gp.sh
@@ -3,14 +3,10 @@
 # prevent read from turning space/tab into \0
 IFS=''
 
-reset=$(tput sgr0)
-dim=$(tput dim)
-clearline=$(tput el)
-savepos=$(tput sc)
-loadpos=$(tput rc)
-
 bins=$(compgen -c)
 input=""
+
+echo -n "$(tput sc)"
 
 while true; do
 
@@ -24,24 +20,33 @@ while true; do
 
     if [[ "$char" == $'\177' ]] # backspace - not \b, \010, ^?, ^H
     then
-        #input=${input::-1}
         input=${input%?}
     elif [[ "$char" != $'\011' ]] # not a tab
     then
         input="${input}${char}"
     fi
 
+    # Find a command that matches the current input
     matches=$(echo "$bins" | grep "^${input}")
     first=$(echo "$matches" | head -n 1)
 
-    if [[ "$char" == $'\011' ]]
+    if [[ "$char" == $'\011' ]] # tab autocomplete
     then
         input="$first"
     fi
 
+    # Display the rest of the first command in grey
     suffix=${first#"$input"}
+    suflen=${#suffix}
 
-    echo -ne "\r${clearline}${input}${savepos}${dim}${suffix}${reset}${loadpos}"
+    # tput cub 0 still moves it back 1, so we don't print anything if suflen is zero
+    if [[ $suflen != 0 ]]; then
+        move="$(tput cub ${suflen})"
+    else
+        move=""
+    fi
+
+    echo -ne "$(tput rc)$(tput el)${input}$(tput dim)${suffix}$(tput sgr0)${move}"
 done
 
 eval $input
